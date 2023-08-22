5fe9d4025d39f943cbca0aaa3443e000f827f9e8
Brian Kipkoech <briankipkoech99@gmail.com>
2023-08-22 17:40:57 +0300

    adding database connection

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
+<?php
+//connect to db
+
+$conn= mysqli_connect('localhost', 'brian', '12345', 'icecream_man' ); 
+//check connection
+if(!$conn){
+    echo 'connection error:' . mysqli_connect_error();
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
-<?php 
-
-include('templates/header.php');
-include('templates/footer.php');
-
-
-?>
+<?php include('templates/header.php');?>
+<?php include('templates/footer.php');?>
     
 </body>
 </html>
